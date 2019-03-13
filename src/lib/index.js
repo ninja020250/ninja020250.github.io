346ca955904c32f466172f682cc0ec7b816cd031
hnhatcuong <hnhatcuong@HNHATCUONG>
2019-03-13 10:48:05 +0700

    test web admin

--- src/lib/index.js
+++
@@ -1,3 +0,0 @@
-import "./jquery";
-import "./bootstrap.js";
-import "./font-awesome.js";
--- src/lib/jquery.js
+++
@@ -1,5 +0,0 @@
-import $ from "jquery";
-
-// config jquery variables for other lib use jQuery (such as bootstrap)
-window.$ = $;
-window.jQuery = $;
