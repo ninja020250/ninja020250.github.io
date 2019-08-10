5b53740c62c3d74da1b5c01aad66ab08e56b63bf
Cuong <nhatcuonghuynh@gmail.com>
2019-08-10 16:13:37 +0700

    remake home page

---
+++ precache-manifest.f6cdd6c20e9730719abc20bfba6a46fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba9147eb1ba23ced779cb60563bc8a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5df798d5de55db79fd26",
+    "url": "/static/css/2.1d6907fe.chunk.css"
+  },
+  {
+    "revision": "7a37467a62b110573289",
+    "url": "/static/css/main.16e723cd.chunk.css"
+  },
+  {
+    "revision": "5df798d5de55db79fd26",
+    "url": "/static/js/2.0759f35b.chunk.js"
+  },
+  {
+    "revision": "7a37467a62b110573289",
+    "url": "/static/js/main.17bdece3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e57b41bcaffc29bc835deea5d4088b6",
+    "url": "/static/media/NEON CLUB MUSIC.0e57b41b.otf"
+  },
+  {
+    "revision": "3f37c5de154bb3ccd811f2f92854139b",
+    "url": "/static/media/SF-UI-Display-Regular.3f37c5de.otf"
+  },
+  {
+    "revision": "45b21fe4f0f7e65308ef52aaf7e8b03b",
+    "url": "/static/media/banner.45b21fe4.jpg"
+  },
+  {
+    "revision": "8c6e69ea9fbf21de82b445c5ab5ed8e0",
+    "url": "/static/media/information-landing.8c6e69ea.jpg"
+  },
+  {
+    "revision": "e1311441d45bfdc224cacd4109ac8f11",
+    "url": "/static/media/logo.e1311441.png"
+  },
+  {
+    "revision": "72d13f72629bf8af122dd90335406174",
+    "url": "/static/media/mobile-landing.72d13f72.jpg"
+  },
+  {
+    "revision": "cda89baececfe22848546d539c9bd826",
+    "url": "/static/media/web-landing.cda89bae.jpg"
+  }
+]);
